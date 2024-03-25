1fe344a58f2c7581bfe2f62b27544fc48272aa33
Ramprosad Gharami <ram.bsmrstu@gmail.com>
2024-03-25 14:02:42 +0600

    Setup cors middleware in the backend.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const route = require("./src/routes/route");
+const corsOptions = require("./src/middlewares/corsMiddleware");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(route);
 
 module.exports = app;
