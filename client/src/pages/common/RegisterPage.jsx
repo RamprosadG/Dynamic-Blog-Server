2221a0e5fb59381cae397de58c782ca7dfa20cff
Ramprosad Gharami <ram.bsmrstu@gmail.com>
2024-04-27 22:56:29 +0600

    Update register page and add validation using formik and yup.

--- client/src/pages/common/RegisterPage.jsx
+++ client/src/pages/common/RegisterPage.jsx
@@ -1,48 +1,37 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
+import { registerSchema } from "../../schema/registerForm";
 
 const RegisterPage = () => {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleUserNameChange = (e) => {
-    setUserName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: registerSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      try {
+        axios
+          .post("http://localhost:5000/register", values)
+          .then((response) => {
+            response.data.success && navigate("/");
+            alert(response.data.message);
+          });
+      } catch (error) {
+        console.log("There is an error", error);
+      }
+    },
+  });
 
   const handleNavigateLoginPage = () => {
     navigate("/login");
-  };
-
-  const handleRegister = async (e) => {
-    //console.log("Register clicked");
-    e.preventDefault();
-    try {
-      const formData = {
-        userName: userName,
-        email: email,
-        password: password,
-      };
-      await axios
-        .post("http://localhost:5000/register", formData)
-        .then((response) => {
-          navigate("/login");
-          alert(response.data.message);
-        });
-    } catch (error) {
-      console.log("There is an error", error);
-    }
   };
 
   return (
@@ -50,46 +39,68 @@ const RegisterPage = () => {
       <div className="d-flex justify-content-center">
         <div className="card card-width my-3">
           <div className="card-body">
-            <div className="card-title text-center">
+            <div className="card-title text-center mb-5">
               <h2>Register</h2>
             </div>
-            <Form.Group className="mb-3" controlId="formBasicUserName">
-              <Form.Label>Enter username</Form.Label>
-              <Form.Control
-                type="text"
-                value={userName}
-                onChange={handleUserNameChange}
-              />
-            </Form.Group>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Enter Email</Form.Label>
+            <Form onSubmit={formik.handleSubmit}>
+              <Form.Group className="mb-4">
                 <Form.Control
+                  id="username"
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                />
+                {formik.touched.username && formik.errors.username ? (
+                  <div>{formik.errors.username}</div>
+                ) : null}
+              </Form.Group>
+
+              <Form.Group className="mb-4">
+                <Form.Control
+                  id="email"
+                  name="email"
                   type="email"
-                  value={email}
-                  onChange={handleEmailChange}
+                  placeholder="Email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div>{formik.errors.email}</div>
+                ) : null}
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Enter Password</Form.Label>
+              <Form.Group className="mb-4">
                 <Form.Control
+                  id="password"
+                  name="password"
                   type="password"
-                  value={password}
-                  onChange={handlePasswordChange}
+                  placeholder="Password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <div>{formik.errors.password}</div>
+                ) : null}
+                <Form.Control.Feedback type="invalid">
+                  Please choose a password.
+                </Form.Control.Feedback>
               </Form.Group>
 
-              <div className="mt-3">
-                <Button variant="outline-secondary" onClick={handleRegister}>
+              <Form.Group className="mt-3">
+                <Button variant="outline-secondary" type="submit">
                   Register
                 </Button>
-              </div>
-              <div className="d-flex justify-content-center mt-4">
+              </Form.Group>
+              <Form.Group className="d-flex justify-content-center mt-4">
                 <Button variant="link" onClick={handleNavigateLoginPage}>
                   Already have an account? Login
                 </Button>
-              </div>
+              </Form.Group>
             </Form>
           </div>
         </div>
