a5fde85e76bd0c57be38ea09017a8dd5db9aaba6
Ashok Raj <ashok.raj4900@gmail.com>
2024-06-06 20:43:06 +0600

    Add allowed origin for netlify deploy.

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dynamic-blog-client.netlify.app",
+];
 
 module.exports = {
   allowedOrigins,
